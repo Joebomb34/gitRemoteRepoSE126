6ddb65039a670efc1bc925c2c665d182d7c887aa
JP Bombassei <jpbombassei@JPs-MacBook-Pro.local>
2021-07-21 16:48:31 -0400

    Lab1

--- Lab1/1C.py
+++ Lab1/1C.py
@@ -11,12 +11,41 @@
 #people - people attending
 #difference - difference of attendies and room capacity
 #-----FUNCTIONS-----
-def capacity():
-    cap = int(input("\t\tWhat is the room capacity?: "))
 
+
+def capacity():
+
+    '''This asks the room cap question'''
+
+    cap = int(input("\t\tWhat is the room capacity?: "))
+    
     return cap
 
+def attendees():
 
+    '''This asks the attending question'''
+
+    people = int(input("\n\t\tHow many people will be attending?: "))
+    
+    return people
+
+def register(cap, people):
+
+    '''This takes the answer from each function and finds the difference'''
+
+    diff = cap - people
+
+    return diff
+
+def again():  
+    
+    '''This asks if you want to remain in the loop'''
+
+    answer = input("Would you like to check any more rooms? [y/n]: ").lower()  
+    
+    return answer
+
+    
 #-----MAIN CODE-----
 answer = "y"
  
@@ -24,22 +53,30 @@ while answer == "y" or answer == "Y":
 
     cap = capacity()
 
-    people = int(input("\n\t\tHow many people will be attending?: "))
-    difference = cap - people
-    sum = people - cap
+    people = attendees()
+
+    difference = register(cap, people)
+   
+    #conditions for the event
 
     if difference >= 0:
         print("The event can be held. {0} more people can attend.".format(difference))
 
+    #when the above is false
+
     else:
-       
-        print("{0} people need to be told they cannot attend.".format(sum))
+        difference = difference * -1
 
-    answer = input("Would you like to check any more rooms? [y/n]: ").lower()
+        print("{0} people need to be told they cannot attend.".format(difference))
 
-    while answer != "y" and answer != "n":
+    answer = again()
+
+    #user trap so the program won't just shut down or error
+
+    while answer != "y" and answer != "n":    
         print("***INVALID OPTION***")
 
-        answer = input("Would you like to check any more rooms? [y/n]: ").lower()
+        answer = input("Would you like to check any more rooms? [y/n]: ").lower()    
+        
 
 print("Thank you for using my program. Goodbye.")
